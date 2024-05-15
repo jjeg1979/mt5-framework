9a0d152d1fd28471587652a5ab93ca8e4fccd865
jjeg1979 <jjeg1979@gmail.com>
2024-05-15 11:19:11 +0200

    Added Signal Interface

---
+++ signal_generator/interfaces/signal_generator_interface.py
@@ -0,0 +1,8 @@
+from typing import Protocol
+
+from events.events import DataEvent
+
+
+class ISignalGenerator(Protocol):
+    def generate_signals(self, data_event: DataEvent) -> None:
+        ...
