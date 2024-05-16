30ba9ea5e095565ba51a5ad993c0ee3928cb833a
jjeg1979 <jjeg1979@gmail.com>
2024-05-16 07:46:23 +0200

    Added FixedSizePositionSizer

---
+++ position_sizer/position_sizers/fixed_size_position_sizer.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+
+from data_provider.data_provider import DataProvider
+from events.events import SignalEvent
+from position_sizer.interfaces.position_sizer_interface import IPositionSizer
+from position_sizer.properties.position_sizer_properties import FixedSizingProps
+
+
+class FixedSizePositionSizer(IPositionSizer):
+    def __init__(self, properties: FixedSizingProps):
+        self.fixed_volume = properties.volume
+
+    def size_signal(
+        self, signal_event: SignalEvent, data_provider: DataProvider
+    ) -> Decimal:
+        # Return a fixed-size position
+
+        return self.fixed_volume
