60fa6771351ce89a3abc59a27c32d95cbc920a61
jjeg1979 <jjeg1979@gmail.com>
2024-05-16 07:45:45 +0200

    Updated position_sizer interface so IPositionSizer returns a Decimal value

--- position_sizer/interfaces/position_sizer_interface.py
+++ position_sizer/interfaces/position_sizer_interface.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Protocol
 
 from data_provider.data_provider import DataProvider
@@ -7,5 +8,5 @@ from events.events import SignalEvent
 class IPositionSizer(Protocol):
     def size_signal(
         self, signal_event: SignalEvent, data_provider: DataProvider
-    ) -> None:
+    ) -> Decimal:
         ...
