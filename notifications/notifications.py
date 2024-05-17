ba81027fda85f94ec767ca72db9f81cf82b27048
jjeg1979 <jjeg1979@gmail.com>
2024-05-17 22:47:18 +0200

    Added TelegramNotificationChannel to notifications service

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -3,18 +3,22 @@ from properties.properties import (
     TelegramNotificationProperties,
 )
 from notifications.interfaces.notifcation_channel import INotificationChannel
+from notifications.channels.telegram_notification_channel import (
+    TelegramNotificationChannel,
+)
 
 
 class NotificationService:
-    def __init__(self, channel: INotificationChannel) -> None:
-        self._channel = channel
+    def __init__(self, properties: NotificationChannelBaseProperties) -> None:
+        self._channel = self._get_channel(properties)
 
     def _get_channel(
         self, properties: NotificationChannelBaseProperties
     ) -> INotificationChannel:
         if isinstance(properties, TelegramNotificationProperties):
-            ...
-        raise NotImplementedError("ERROR: Communication Channel not implemented")
+            return TelegramNotificationChannel(properties)
+
+        raise ValueError("ERROR: The communication channel does not exist.")
 
     def send_notification(self, title: str, message: str) -> None:
         self._channel.send_message(title, message)
