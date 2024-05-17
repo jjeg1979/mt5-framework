6c81357132e21b5c850f7aeb7abebb9a74d65a8c
jjeg1979 <jjeg1979@gmail.com>
2024-05-17 09:35:25 +0200

    Include Risk Manager in Trading App

--- trading_app.py
+++ trading_app.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from queue import Queue
 from typing import Any
 
@@ -6,8 +7,12 @@ from data_provider.data_provider import DataProvider
 from portfolio.portfolio import Portfolio
 from position_sizer.position_sizer import PositionSizer
 from position_sizer.properties.position_sizer_properties import (
-    MinSizingProps,
+    FixedSizingProps,
+    # MinSizingProps,
 )
+
+from risk_manager.properties.risk_manager_properties import MaxLeverageFactorRiskProps
+from risk_manager.risk_manager import RiskManager
 from trading_director.trading_director import TradingDirector
 
 from signal_generator.signals.signal_ma_crossover import SignalMACrossover
@@ -51,7 +56,7 @@ def main() -> None:
         events_queue, data_provider, portfolio, timeframe, fast_ma_pd, slow_ma_pd
     )
 
-    sizing_properties = MinSizingProps()
+    sizing_properties = FixedSizingProps(volume=Decimal(500))
 
     position_sizer = PositionSizer(
         events_queue=events_queue,
@@ -59,12 +64,21 @@ def main() -> None:
         sizing_properties=sizing_properties,
     )
 
+    risk_properties = MaxLeverageFactorRiskProps(max_leverage_factor=Decimal(5.0))
+
+    risk_manager = RiskManager(
+        events_queue=events_queue,
+        data_provider=data_provider,
+        portfolio=portfolio,
+        risk_properties=risk_properties,
+    )
     # Create the trading director and start the main loop
     trading_director: TradingDirector = TradingDirector(
         events_queue=events_queue,
         data_provider=data_provider,
         signal_generator=signal_generator,
         position_sizer=position_sizer,
+        risk_manager=risk_manager,
     )
     trading_director.execute()
 
